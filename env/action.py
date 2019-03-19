30a3a5228e923fcd6fe7180cdb6b32aa278e17cd
JianWang <yt4766269@126.com>
2019-03-19 17:27:49 +0800

    env

---
+++ env/action.py
@@ -0,0 +1,14 @@
+action = ['delete_node', 'delete_edge', 'filter', 'find_path']
+
+delete_node_action = ['0', '1', '2', '3', '4', '5']
+
+delete_edge_action = [('0', '1'), ('0', '3'), ('1', '2'), ('2', '3'), ('3', '4'), ('4', '5'), ('4', '1'), ('5', '0'), ('5', '2')]
+
+# filter action means filter path from the graph
+filter_action = [['0', '1', '2', '3'], ['5', '0', '1']]
+
+find_path_condition = ['Delay < 20', "HopCount < 5", "Delay < 15"]
+
+find_path_source = ['0', '1', '2', '3', '4', '5']
+
+find_path_target = ['0', '1', '2', '3', '4', '5']
--- env/network.py
+++ env/network.py
@@ -1,7 +1,18 @@
 import networkx as nx
 import json
 import matplotlib.pyplot as plt
+import env.action as action
+
 class Network:
+
+    def get_edges_from_path(self, path):
+        edges = []
+        for i in range(len(path) - 1):
+            source = path[i]
+            target = path[i + 1]
+            edges.append((source, target))
+        return edges
+
     def __init__(self, network_path):
         self.G = nx.DiGraph()
 
@@ -11,15 +22,38 @@ class Network:
             self.G.add_node(item['Id'], delay=item['Properties']['Delay'], hopcount=item['Properties']['HopCount'])
         for item in network_json['Links']:
             self.G.add_edge(item['Source'], item['Target'], bandwidth=item['Properties']['Bandwidth'],
-                            cost=item['Properties']['Cost'], delay=item['Properties']['Delay'],)
-        p = nx.shortest_path(self.G, "0", "5", "Delay")
-        print(p[0])
+                            cost=item['Properties']['Cost'], delay=item['Properties']['Delay'], )
 
-        sub_g = nx.subgraph(self.G, p)
+    def delete_node(self, node):
+        self.G.remove_node(node)
+        return self.G.edges
 
-        nx.draw(sub_g, with_labels=True, )
-        plt.show()
+    def delete_edge(self, edge):
+        source = edge[0]
+        target = edge[1]
+        self.G.remove_edge(source, target)
+        return self.G.edges
+
+    def filter(self, path):
+        edges = self.get_edges_from_path(path)
+        self.G.remove_edges_from(edges)
+        return self.G.edges
+
+    # def find_path(self, source, target, condition):
+    #     result = None
+    #     for path in nx.all_simple_paths(self.G, source, target):
+    #         condition = condition.split(' ')
+    #         attribute = condition[0]
+    #         num = condition[2]
+
+    def find_path(self, source, target):
+        p = nx.shortest_path(self.G, source, target)
+        edges = self.get_edges_from_path(p)
+        return edges
 
 
 if __name__ == '__main__':
     network = Network('default_network.json')
+    edges = network.__getattribute__(action.action[1])(action.delete_edge_action[1])
+    edges = network.__getattribute__(action.action[3])(action.find_path_source[0], action.find_path_target[4])
+    print(edges)
